ff67507d0c022e85e104013b3c07745b75a2bead
AL AKHRAS Walid <walid.alakhras@qiminfo.ch>
2023-11-26 17:12:21 +0100

    add s3 utils

--- data_ingestion/handler.py
+++ data_ingestion/handler.py
@@ -2,18 +2,21 @@ import json, os, requests, boto3
 from utils import s3_utils
 
 url = 'https://eforexcel.com/wp/wp-content/uploads/2020/09/2m-Sales-Records.zip'
+bucket_name = os.getenv('BUCKET_NAME')
 
 def lambda_handler(event, context):
    
 
     res = requests.get(url)
 
+    if res.status_code == 200:
+        s3_utils.put_object(bucket_name, 'RAW/file.zip', body=res.content)
 
 
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps('body')
     }
 
     return response
